52b22c0f962daa1a16f45841e73391eaa43b203a
Ping-Lin <billy3962@hotmail.com>
2014-02-23 23:25:01 +0800

    add Hump

--- Test.cpp
+++ Test.cpp
@@ -8,24 +8,32 @@
 #include "Constants.h"
 #include "Block.h"
 #include "Track.h"
+#include "BlockToTrack.h"
+#include "BlockValues.h"
 using namespace std;
 using namespace Constants;
 
-enum EventTime{tr, th, tc, tp1, tp2, to};   //tr=event time at receivingArea...h=hump, c=classification, p1=pullback1, p2=pullback2, o=outboundArea
+enum EventTime{tr, th, tp1, tp2, to};   //tr=event time at receivingArea...h=hump, c=classification, p1=pullback1, p2=pullback2, o=outboundArea
 vector<double> eventTime[6];   //time for event time(like the )
 double latestTime = 0.0f;   //save the events latest time
 Track *receivingTracks, *classificationTracks, *outboundTracks;   //feature same as the name
+
+//for print out
+vector< vector<Block> >humpBlockList;   //print inbound_train_info
 void readInboundCar(vector< vector<Block> > &);   //read the csv file to save the railcar information
 
 //every event function
 double receivingTrain(vector <vector<Block> > &);   //receive the train, argument is the blocklist
 double humpTrain();   //hump the train
-double classifyTrain();   //classify the train
 double pullBackTrain(int);   //pull back the train, argument is pullback No. 
 double goOutTrain();   //go out train
 
 //receivingArea function
 int findLongestTrainAtBlockList(vector< vector<Block> >, double);   //find the longest train at blocklist that is inside the limits, return the id of that train
+
+//hump function
+vector<int> findLongestAtReceivingArea();   //find the longest train at receiving area and return the train id sequence from long to short
+bool putIntoClassificationArea(vector<Block>);
 
 int main(){
     ios::sync_with_stdio(false);   //stop the integration of stdio and printf, scanf
@@ -39,19 +47,19 @@ int main(){
     outboundTracks = new Track[DEPARTURE_TRACKS_NUMBER];
     
     //time array to save every event last time
-    for(int i=0 ; i<6 ; i++){
+    for(int i=0 ; i<5 ; i++){
 	eventTime[i].push_back(0.0f);   //initial
     }
     latestTime = blockList[0][0].timeAtReceivingArea;   //set the first time
     
     double tmpTime;
-    int eventIdSequence[6];
+    int eventIdSequence[5];
     //do the events
     while(1){
 	//select the smallest time, using insertion sort to save all the id sort, because if the smallest time event can't do, 
 	//than do the second small event, etc., save the id sequence into eventIdSequence
 	eventIdSequence[0] = 0;
-	for(int i=1 ; i<6 ; i++){
+	for(int i=1 ; i<5 ; i++){
 	    eventIdSequence[i] = i;
 	    tmpTime = eventTime[eventIdSequence[i]].back();
 	    int j;
@@ -66,16 +74,13 @@ int main(){
 	
 	//select which event to do by the eventIdSequence
 	bool ifBreak = false;
-	for(int i=0 ; i<6 ; i++){
+	for(int i=0 ; i<5 ; i++){
 	    switch(eventIdSequence[i]){
 		case tr:   //receivingArea
 		    tmpTime = receivingTrain(blockList);
 		    break;
 		case th:   //hump
 		    tmpTime = humpTrain();
-		    break;
-		case tc:   //classification
-		    tmpTime = classifyTrain();
 		    break;
 		case tp1:   //pullback 1
 		    tmpTime = pullBackTrain(1);
@@ -105,8 +110,6 @@ int main(){
 	if(ifBreak==false)
 	    break;
     }
-    /*for(int i=0 ; i<RECEIVING_TRACKS_NUMBER ; i++)
-	cout << receivingTracks[i].train[0].trainId << endl;*/
 }
 
 /*
@@ -152,7 +155,7 @@ double receivingTrain(vector< vector<Block> > &bl){
     else if(id==-2){   //there are some empty tracks, but time limit so can't go in
 	int maxLength = 0;
 	id=0;
-	for(int i=0 ; ; i++){
+	for(int i=0 ; bl.size() ; i++){
 	    if((bl[i][0].timeAtReceivingArea <= latestTime)){
 		if(bl[i].size() > maxLength){
 		    maxLength = bl[i].size();
@@ -164,31 +167,84 @@ double receivingTrain(vector< vector<Block> > &bl){
 	}
     }
     //do the event to save the train
+    double minHumpT = INT_MAX;
+    int minId;
+    bool ifSuccess=false;
     for(int i=0 ; i<RECEIVING_TRACKS_NUMBER ; i++){
 	if(receivingTracks[i].ifEmpty == true){
-	    if(bl[id][0].timeAtReceivingArea < receivingTracks[i].humpTime){
-		//update time
-		for(vector<Block>::iterator it=bl[id].begin() ; it != bl[id].end() ; ++it){
-		    (*it).timeAtReceivingArea = receivingTracks[i].humpTime;
-		}
+	    if(minHumpT > receivingTracks[i].humpTime){
+		minHumpT = receivingTracks[i].humpTime;
+		minId = i;
 	    }
-	    receivingTracks[i].ifEmpty = false;
-	    receivingTracks[i].train = bl[id];
-	    bl.erase(bl.begin()+id);
-
-	    cout << "[ReceivingArea Time]: " << receivingTracks[i].train[0].timeAtReceivingArea << ", Train id: " << receivingTracks[i].train[0].trainId << endl;
-	    return receivingTracks[i].train[0].timeAtReceivingArea;
+	    if(bl[id][0].timeAtReceivingArea >= receivingTracks[i].humpTime){
+		receivingTracks[i].ifEmpty = false;
+		receivingTracks[i].train = bl[id];
+		bl.erase(bl.begin()+id);
+		ifSuccess = true;
+		minId = i;
+		break;
+	    }
 	}
-    } 
-    return eventTime[tr].back();
-}
+    }
+    if(ifSuccess==false){
+	if(bl[id][0].timeAtReceivingArea < receivingTracks[minId].humpTime){
+	    //update time
+	    for(vector<Block>::iterator it=bl[id].begin() ; it != bl[id].end() ; ++it){
+		(*it).timeAtReceivingArea = receivingTracks[minId].humpTime;
+	    }
+	}
+	receivingTracks[minId].ifEmpty = false;
+	receivingTracks[minId].train = bl[id];
+	bl.erase(bl.begin()+id);
+    }
 
+    cout << "[ReceivingArea Time]: " << receivingTracks[minId].train[0].timeAtReceivingArea << ", Train id: " << receivingTracks[minId].train[0].trainId << ", Track: R" << minId+1 << endl;
+    return receivingTracks[minId].train[0].timeAtReceivingArea;
+}
+/*
+* hump the train from receivingArea to classification area
+* need the function: vector<int> findLongestAtReceivingArea(), bool putIntoClassificationArea(vector<Block>);
+*/
 double humpTrain(){
-    return 0.0f;
-}
-
-double classifyTrain(){
-    return 0.0f;
+    vector<int> id = findLongestAtReceivingArea();
+    double time;
+    vector<Block> tmpList;
+    if(id[0]==-1)
+	return eventTime[th].back();
+    else{
+	for(int i=0 ; i<id.size() ; i++){   //from long to short
+	    time = receivingTracks[id[i]].train[0].timeAtReceivingArea + TECHNICAL_INSPECTION_TIME;   //add technical time
+	    if(eventTime[th].back()+HUMP_INTERVAL <= time)   //count the time
+		time = time + HUMP_INTERVAL;
+	    else
+		time = eventTime[th].back() + HUMP_INTERVAL;
+	    for(vector<Block>::iterator it=receivingTracks[id[i]].train.begin() ; it!=receivingTracks[id[i]].train.end() ; ++it){
+		(*it).timeStartHump = time;
+		(*it).timeEndHump = time + HUMP_RATE*receivingTracks[id[i]].train.size();
+	    }
+	    if(putIntoClassificationArea(receivingTracks[id[i]].train) == false){   //receive error
+		continue;
+	    }
+	    else{   //receive success
+		for(vector<Block>::iterator b=receivingTracks[id[i]].train.begin() ; b!=receivingTracks[id[i]].train.end() ; ++b){
+		    tmpList.push_back((*b));
+		}
+		//eventTime[th].push_back(time);
+		cout << "[Hump]Start: " << time << endl;
+		receivingTracks[id[i]].humpTime = time;
+		time+=HUMP_RATE*receivingTracks[id[i]].train.size();
+		receivingTracks[id[i]].train.clear();
+		vector<Block>(receivingTracks[id[i]].train).swap(receivingTracks[id[i]].train);
+		receivingTracks[id[i]].ifEmpty = true;
+		
+		humpBlockList.push_back(tmpList);
+		
+		return time;
+	    }
+	}
+	cout << "[Hump]classification tracks are full." << endl;
+    }
+    return eventTime[th].back();
 }
 
 double pullBackTrain(int no){
@@ -213,10 +269,10 @@ int findLongestTrainAtBlockList(vector< vector<Block> >bl, double t){
 	    minHumpTime = receivingTracks[i].humpTime;
 	}
     }
-    if(minHumpTime == INT_MAX)   //no empty train
+    if(minHumpTime == INT_MAX)   //no empty track
 	return id;
 
-    for(int i=0 ; ; i++){
+    for(int i=0 ; i<bl.size() ; i++){
 	if((bl[i][0].timeAtReceivingArea <= t) && (bl[i][0].timeAtReceivingArea >= minHumpTime)){
 	    if(bl[i].size() > maxLength){
 		maxLength = bl[i].size();
@@ -230,5 +286,91 @@ int findLongestTrainAtBlockList(vector< vector<Block> >bl, double t){
 	    break;
 	}
     }
+    return id;   //no train coming
+}
+
+/**
+* find the longest train at receiving at receiving area, because maybe hump can't move the longest train,
+* so give the track id sequence of size, from long to short
+* return -1 if no train can hump
+*/
+vector<int> findLongestAtReceivingArea(){
+    int maxLength = 0;
+    vector<int> id;   //track id sequence for size, long to short
+    vector<int>::iterator it = id.begin();
+    for(int i=0 ; i<RECEIVING_TRACKS_NUMBER ; i++){
+	int count = id.size();
+	for(int j=id.size()-1 ; j>=0 ; j--){
+	    if((receivingTracks[i].ifEmpty==false)){
+		if(receivingTracks[i].train.size() > receivingTracks[id[j]].train.size())
+		    count = j;
+	    }
+	}
+	if(receivingTracks[i].ifEmpty == false){
+	    it = id.begin();
+	    id.insert(it+count, i);
+	}
+    }
+     
+    if(id.empty()){
+	id.push_back(-1);
+    }
     return id;
+}
+/*
+* classify the train into classification area, if success return true, else return false
+* argument: the train
+*/
+bool putIntoClassificationArea(vector<Block> bl){
+    bool ifAdd = false;
+    vector<int> addId;
+    for(vector<Block>::iterator b=bl.begin() ; b!=bl.end() ; ++b){
+	ifAdd = false;
+	for(int i=0 ; i<CLASSIFICATION_TRACKS_NUMBER ; i++){
+	    if((classificationTracks[i].ifEmpty == false) && (classificationTracks[i].train.size()+1 <= CLASSIFICATION_TRACKS_CAPACITY)){
+		if(classificationTracks[i].train[0].blockName.compare((*b).blockName)==0){   //if the same
+		    classificationTracks[i].train.push_back((*b));
+		    ifAdd = true;
+		    addId.push_back(i);
+		    classificationTracks[i].btt.back().endingTime = (*b).timeEndHump;
+		    break;
+		}
+	    }
+	}
+	if(ifAdd==false){
+	    for(int i=0 ; i<CLASSIFICATION_TRACKS_NUMBER ; i++){
+		if(classificationTracks[i].ifEmpty==true){
+		    classificationTracks[i].train.push_back((*b));
+		    cout << "[Hump]: new track: " << classificationTracks[i].train[0].blockName << endl;
+		    classificationTracks[i].ifEmpty = false;   //a new track to save
+		    
+		    if(classificationTracks[i].btt.size()>0)
+			classificationTracks[i].btt.back().endingTime = (*b).timeStartHump-HUMP_INTERVAL;
+
+		    BlockToTrack tmp;
+		    tmp.trackNo = i;
+		    tmp.blockName = (*b).blockName;
+		    tmp.dayNo = ((int)((*b).timeStartHump-1))/24+1;
+		    tmp.startingTime = (*b).timeStartHump-HUMP_INTERVAL;
+		    classificationTracks[i].btt.push_back(tmp);
+
+		    ifAdd = true;
+		    addId.push_back(i);
+		    break;
+		}
+	    }
+	}
+
+	if(ifAdd == false){
+	    for(vector<int>::iterator it=addId.begin() ; it!=addId.end() ; ++it){
+		classificationTracks[(*it)].train.pop_back();
+	    }
+	    for(int i=0 ; i<CLASSIFICATION_TRACKS_NUMBER ; i++){
+		if(classificationTracks[i].train.empty())
+		    classificationTracks[i].ifEmpty = true;
+	    }
+	    return false;
+	}
+    }
+    return true;
 }
